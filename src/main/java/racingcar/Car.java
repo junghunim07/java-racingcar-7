9df1f67fa316c53b6f8e4211f362f5de5af15d2b
imjunghun <dlawndgns9@naver.com>
2024-11-24 19:29:07 +0900

    refactor: 이름 검증 책임 Name으로 전가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,34 +5,16 @@ public class Car {
     private static final int MOVE_THRESHOLD = 4;
     private static final int STEP = 1;
 
-    String name;
+    Name name;
     int position = 0;
 
-    Car(String name) {
-        validate(name);
+    Car(Name name) {
         this.name = name;
     }
 
     void move(int condition) {
         if (condition >= MOVE_THRESHOLD) {
             this.position += STEP;
-        }
-    }
-
-    private void validate(String name) {
-        validateLength(name);
-        validateBlank(name);
-    }
-
-    private void validateLength(String name) {
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("6자 이상은 입력할 수 없습니다.");
-        }
-    }
-
-    private void validateBlank(String name) {
-        if (name.isBlank()) {
-            throw new IllegalArgumentException("이름은 공백일 수 없습니다.");
         }
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,45 +1,17 @@
 package racingcar;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
 
-    @DisplayName("자동차는 5자 이하의 이름을 가질 수 있다.")
-    @Test
-    void 자동차_5자_이하_이름() {
-        String name = "자바스프링";
-
-        Car car = new Car(name);
-
-        assertThat(car.name).isEqualTo(name);
-    }
-
-    @DisplayName("자동차 6자 이상일 경우 예외 발생")
-    @Test
-    void 자동차_6자_이상_예외() {
-        String name = "백엔드개발자";
-
-        assertThatThrownBy(() -> new Car(name))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("자동차 이름이 공백일 경우 예외")
-    @Test
-    void 자동차_이름_공백_예외() {
-        String name = "";
-
-        assertThatThrownBy(() -> new Car(name))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
     @DisplayName("자동차는 위치 값을 가진다.")
     @Test
     void 자동차_위치() {
-        Car car = new Car("자동차");
+        Name name = new Name("자동차");
+        Car car = new Car(name);
 
         assertThat(car.position).isEqualTo(0);
     }
@@ -47,7 +19,8 @@ public class CarTest {
     @DisplayName("자동차는 0에서 9 사이에서 무작위 값을 구한 후 값이 4 이상일 경우 전진한다.")
     @Test
     void 자동차_전진() {
-        Car car = new Car("자동차");
+        Name name = new Name("자동차");
+        Car car = new Car(name);
 
         car.move(4);
 
@@ -57,7 +30,8 @@ public class CarTest {
     @DisplayName("자동차는 0에서 9 사이에서 무작위 값을 구한 후 값이 3 이상일 경우 전진하지 않는다.")
     @Test
     void 자동차_정지() {
-        Car car = new Car("자동차");
+        Name name = new Name("자동차");
+        Car car = new Car(name);
 
         car.move(0);
 
